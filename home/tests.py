0a901ed06a2e1f53a4b12dce427f598bf6a4ca04
sooyuan <kongsooyuan@gmail.com>
2025-10-16 17:11:24 -0600

    feat: Add index page unit tests to meet 80% coverage

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    """Tests the basic functionality of the landing page."""
+
+    def test_view_url_exists_at_proper_location(self):
+        """Test that the homepage resolves to the root URL (/)."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        """Test that the correct template (index.html) is used."""
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'index.html')
