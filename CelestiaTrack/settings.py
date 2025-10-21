c9f5cd282f8a628b582332b0d4402389f7670785
dyladude <36177721+dyladude@users.noreply.github.com>
2025-10-20 21:35:00 -0600

    Add WhiteNoise middleware and STATIC_ROOT setting
    Added WhiteNoise middleware for static file handling and defined STATIC_ROOT for production.

--- CelestiaTrack/settings.py
+++ CelestiaTrack/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", # So render works
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# where collectstatic will put files for production
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_DIRS = [ BASE_DIR / "home" / "static", ] # looks inside home/static/
 
 # Default primary key field type
