871d129b9b619d9362997bb9d2eaea9c2fbfd92b
sooyuan <kongsooyuan@gmail.com>
2025-12-09 15:58:52 -0700

    Fix linting errors: remove unused imports and fix formatting

--- home/views.py
+++ home/views.py
@@ -17,7 +17,6 @@ from django.contrib import messages
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.conf import settings
 from django import forms
 from django.views.decorators.http import require_http_methods, require_GET
 
